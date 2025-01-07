4e010f4b581ab4f8e1e312a179637c2beb541ee4
Jarrod van der Wal <jarvanderwal@gmail.com>
2025-01-07 14:37:25 +1100

    Added check_valid_file_path() alongside tests.

--- R/universal_helpers.R
+++ R/universal_helpers.R
@@ -70,3 +70,51 @@ attempt_horizontal_tab_data <- function(tidyxl_data, formatting_data) {
     return(results$result)
   }
 }
+
+#' Check whether a file path is valid and throw an error if not.
+#'
+#' @param file_path the file path to check
+#'
+#' @keywords internal
+#'
+check_valid_file_path <- function(file_path) {
+  if (!is.character(file_path)) {
+    cli::cli_abort(
+      message = c(
+        "{.arg file_path} must be a string of length 1.",
+        c("x" = "Supplied argument is of class {.cls {class(file_path)}}.")
+      ),
+      class = "read_apra_error_file_path_not_string"
+    )
+  }
+
+  if (length(file_path) != 1) {
+    cli::cli_abort(
+      message = c(
+        "{.arg file_path} must be a string of length 1.",
+        c("x" = "Supplied argument is of length {.val {length(file_path)}}.")
+      ),
+      class = "read_apra_error_file_path_not_length_one"
+    )
+  }
+
+  suppressWarnings(normalised_file_path <- normalizePath(file_path))
+
+  if (!file_exists_wrapper(normalised_file_path)) {
+    cli::cli_abort(
+      message =
+        c(
+          c("x" = "No such file exists at: {.path {normalised_file_path}}."),
+          c("!" = "Please check you have specified the correct file path.")
+        ),
+      class = "read_apra_error_file_path_does_not_exist"
+    )
+  }
+}
+
+#' Wrapper used for testing purposes
+#'
+#' @keywords internal
+#'
+file_exists_wrapper <- file.exists
+
--- tests/testthat/test-universal_helpers.R
+++ tests/testthat/test-universal_helpers.R
@@ -10,3 +10,23 @@ test_that("attempt_horizontal_tab_data()", {
 
   expect_equal(test_output, expected_output)
 })
+
+test_that("check_valid_file_path()", {
+
+  expect_error(
+    check_valid_file_path(file_path = 123),
+    class ="read_apra_error_file_path_not_string"
+  )
+
+  expect_error(
+    check_valid_file_path(file_path = c("a", "b")),
+    class ="read_apra_error_file_path_not_length_one"
+  )
+
+  local_mocked_bindings(file_exists_wrapper = function(...) FALSE)
+
+  expect_error(
+    check_valid_file_path(file_path = "C:/Users/"),
+    class = "read_apra_error_file_path_does_not_exist"
+  )
+})
