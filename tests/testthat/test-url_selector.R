fe46c221520493ba90400c8bff8e66ed1e577b41
Jarrod van der Wal <jarvanderwal@gmail.com>
2025-01-12 23:05:41 +1100

    Added tests for url_selector.R functions.

---
+++ tests/testthat/test-url_selector.R
@@ -0,0 +1,42 @@
+test_that("url_selector() behaves as expected", {
+
+  input_urls_to_check <-
+    c(
+      "#main-content",
+      "https://www.apra.gov.au/",
+      "https://www.apra.gov.au/news-and-publications?industry%5B2%5D=2",
+      "https://www.apra.gov.au/sites/default/files/2024-10/20241031%20-%20Monthly%20authorised%20deposit-taking%20institution%20statistics%20back-series%20March%202019%20-%20September%202024.xlsx",
+      "https://www.apra.gov.au/sites/default/files/monthly_banking_statistics_june_2019_back_series.xlsx",
+      "https://www.apra.gov.au/sites/default/files/2024-09/20240912%20-%20Quarterly%20authorised%20deposit-taking%20institution%20performance%20statistics%20-%20September%202004%20to%20June%202024.xlsx",
+      "https://www.apra.gov.au/sites/default/files/2024-09/20240912%20-%20Quarterly%20authorised%20deposit-taking%20institution%20property%20exposures%20statistics%20-%20September%202004%20to%20June%202024.xlsx",
+      "https://www.apra.gov.au/sites/default/files/2020-03/Quarterly%20authorised%20deposit-taking%20institution%20property%20exposures%20-%20Historical%20data.xlsx",
+      "https://www.apra.gov.au/sites/default/files/2024-09/20240912%20-%20Authorised%20deposit-taking%20institution%20centralised%20publication%20-%20March%202013%20to%20June%202024_0.xlsx",
+      "https://www.apra.gov.au/sites/default/files/2024-10/20241016%20-%20Authorised%20deposit-taking%20institutions%20points%20of%20presence%20June%202017%20to%20June%202024.xlsx"
+    )
+
+  input_selected_stat_pub <-
+    dplyr::filter(
+      .data = apra_stat_pub_details,
+      publication == "madis", cur_hist == "current"
+      )
+
+  # Regex behaves as expected
+  expect_equal(
+    url_selector(input_urls_to_check, input_selected_stat_pub),
+    input_urls_to_check[4]
+  )
+
+  # Stringdist accounts for minor spelling error
+  input_urls_to_check[4] <- "https://www.apra.gov.au/sites/default/files/2024-10/20241031%20-%20Month%20authorised%20deposi-taking%20instiution%20%20back-series%20March%202019%20-%20September%202024.xlsx"
+  expect_equal(
+    url_selector(input_urls_to_check, input_selected_stat_pub),
+    input_urls_to_check[4]
+  )
+})
+
+test_that("prepare_name_comparison() behaves as expected", {
+  expect_equal(
+    prepare_name_comparison("20241212%20-%20Quarterly%20authorised%20deposit-taking%20institution%20performance-September%202004%20to%20September%202024.xlsx"),
+    "- Quarterly authorised deposit-taking institution performance-  to  .xlsx"
+  )
+})
