f32cf1a1d7ef4685808d9388fb531d677f54d0e5
Jarrod van der Wal <jarvanderwal@gmail.com>
2025-01-12 22:48:35 +1100

    Added/reworked tests for download_apra.R

--- tests/testthat/test-download_apra.R
+++ tests/testthat/test-download_apra.R
@@ -1,103 +1,215 @@
-test_that("download_apra()", {
-  local_mocked_bindings(polite_bow = function(...) "bow_object")
-  local_mocked_bindings(sys_sleep_wrapper = function(...) NULL)
+test_that("download_apra() real file download", {
+  skip_if_offline()
+  skip_on_cran()
 
-  test_url_strings <-
-    c(
-      "https://www.apra.gov.au/sites/default/files/2024-12/20241212%20-%20Quarterly%20authorised%20deposit-taking%20institution%20performance%20statistics%20-%20September%202004%20to%20September%202024.xlsx",
-      "https://www.apra.gov.au/sites/default/files/2024-12/20241212%20-%20Authorised%20deposit-taking%20institution%20centralised%20publication%20-%20March%202013%20to%20September%202024.xlsx",
-      "https://www.apra.gov.au/sites/default/files/2024-12/20241212%20-%20Quarterly%20authorised%20deposit-taking%20institution%20property%20exposures%20statistics%20-%20September%202004%20to%20September%202024.xlsx",
-      "https://www.apra.gov.au/sites/default/files/2020-03/Quarterly%20authorised%20deposit-taking%20institution%20property%20exposures%20-%20Historical%20data.xlsx"
-    )
-
-  # Testing working case
-  local_mocked_bindings(safely_download_file = function(...) list(result = "returned_file_path", error = NULL))
-  local_mocked_bindings(scrape_urls = function(...) test_url_strings)
-
-  expect_equal(
-    download_apra(
-      publication = "qadips",
-      cur_hist = "current",
-      backup_match = "performance",
-      backup_remove = NULL
-    ),
-    "returned_file_path"
+  # Test correct QADIPS download
+  expect_true(
+    file.exists(download_apra(publication = "qadips", cur_hist = "current", quiet = TRUE))
   )
 
-  # Testing could not download case
-  local_mocked_bindings(safely_download_file = function(...) list(result = NULL, error = "returned_error"))
-  local_mocked_bindings(get_http_status = function(...) "Client error: (404) Not Found")
-
-  expect_error(
-    download_apra(
-      publication = "qadips",
-      cur_hist = "current",
-      backup_match = "performance",
-      backup_remove = NULL
-    ),
-    class = "read_apra_error_could_not_download"
-  )
-
-  # Testing no performance string
-  local_mocked_bindings(scrape_urls = function(...) test_url_strings[-1])
-
-  expect_error(
-    download_apra(
-      publication = "qadips",
-      cur_hist = "current",
-      backup_match = "performance",
-      backup_remove = NULL
-    ),
-    class = "read_apra_error_back_url_no_matches"
-  )
-
-  # Testing duplicate performance string
-  local_mocked_bindings(scrape_urls = function(...) c(test_url_strings, test_url_strings[1]))
-
-  expect_error(
-    download_apra(
-      publication = "qadips",
-      cur_hist = "current",
-      backup_match = "performance",
-      backup_remove = NULL
-    ),
-    class = "read_apra_error_back_url_multiple_matches"
+  # Test correct MADIS download
+  expect_true(
+    file.exists(download_apra(publication = "madis", cur_hist = "historic", quiet = TRUE))
   )
 })
 
-test_that("backup_link_identifier()", {
-  example_urls <-
-    c(
-      "https://www.apra.gov.au/sites/default/files/2024-12/20241212%20-%20Quarterly%20authorised%20deposit-taking%20institution%20performance-September%202004%20to%20September%202024.xlsx",
-      "https://www.apra.gov.au/sites/default/files/2024-12/20241212%20-%20Authorised%20deposit-taking%20institution%20centralised%20publication%20-%20March%202013%20to%20September%202024.xlsx",
-      "https://www.apra.gov.au/sites/default/files/2024-12/20241212%20-%20Quarterly%20authorised%20deposit-taking%20institution%20property%20exposures%20statistics%20-%20September%202004%20to%20September%202024.xlsx",
-      "https://www.apra.gov.au/sites/default/files/2020-03/Quarterly%20authorised%20deposit-taking%20institution%20property%20exposures%20-%20Historical%20data.xlsx"
+test_that("download_apra() error handling", {
+  # Wrong publication argument
+  expect_error(download_apra(publication = "NULL", cur_hist = "current"))
+
+  # Wrong cur_hist argument
+  expect_error(download_apra(publication = "qadips", cur_hist = "NULL"))
+
+  # Error if can't get connection
+  local_mocked_bindings(
+    get_http_status = function(...) {
+      list(
+        category = "Client error",
+        reason = "Not Found",
+        message = "Client error: (404) Not Found"
+      )
+    }
+  )
+
+  expect_error(
+    download_apra(publication = "qadips", cur_hist = "current"),
+    regexp = "Could not scrape url to download from:",
+    class = "readapra_error_http_status_error"
+  )
+
+  # Error if can't download
+  local_mocked_bindings(
+    get_http_status = function(...) {
+      list(
+        category = "Success",
+        reason = "OK",
+        message = "Success: (200) OK"
+      )
+    }
+  )
+  local_mocked_bindings(bow_wrapper = function(...) NULL)
+  local_mocked_bindings(scrape_urls = function(...) NULL)
+  local_mocked_bindings(url_selector = function(...) NULL)
+  local_mocked_bindings(sys_sleep_wrapper = function(...) NULL)
+  local_mocked_bindings(safe_polite_file_download = function(...) {
+    list(result = NULL, error = "Error")
+  })
+
+  expect_error(
+    download_apra(publication = "qadips", cur_hist = "current"),
+    regexp = "Could not download file from:",
+    class = "readapra_error_could_not_download_file"
+  )
+})
+
+test_that("scrape_urls() behaves as expected", {
+  enable(quiet = TRUE)
+
+  mock_html <-
+    "
+<html>
+  <body>
+    <a href='https://example.com/link1'>Link 1</a>
+    <a href='https://example.com/link2'>Link 2</a>
+  </body>
+</html>
+"
+
+  to_return(
+    .data = stub_request("get", "https://example.com"),
+    body = mock_html,
+    status = 200,
+    headers = list("Content-Type" = "text/html; charset=utf-8")
+  )
+
+  wi_th(
+    .data = stub_request("get", uri = "https://example.com/robots.txt"),
+    headers = list(
+      "Accept" = "application/json, text/xml, application/xml, */*",
+      "user-agent" = "polite R package"
     )
-
-  # Correctly selecting the QADIP
-  expect_equal(
-    backup_link_identifier(example_urls, "performance"), example_urls[1]
   )
 
-  # Correctly selecting the current QPEX
-  expect_equal(
-    backup_link_identifier(example_urls, "property|exposures", "historic"), example_urls[3]
-  )
+  session <- polite::bow("https://example.com")
+  urls <- scrape_urls(session)
 
-  # Correctly selecting the historic QPEX
-  expect_equal(
-    backup_link_identifier(example_urls, "property|exposures|historic"), example_urls[4]
-  )
+  expect_equal(urls, c("https://example.com/link1", "https://example.com/link2"))
 
-  # Not identifying any matches
+  disable(quiet = TRUE)
+})
+
+test_that("check_http_status() behaves as expected", {
+  enable(quiet = TRUE)
+
+  to_return(.data = stub_request("get", "https://example.com/success"), status = 200)
+  to_return(.data = stub_request("get", "https://example.com/notfound"), status = 404)
+  to_return(.data = stub_request("get", "https://example.com/servererror"), status = 501)
+
+  expect_no_error(check_http_status("https://example.com/success"))
+
   expect_error(
-    backup_link_identifier(example_urls, "not_found"),
-    class = "read_apra_error_back_url_no_matches"
+    check_http_status(
+      url = "https://example.com/notfound",
+      error_message = "Encountered error:"
+    ),
+    regexp = "Encountered error: ",
+    class = "readapra_error_http_status_error"
   )
 
-  # Multiple evenly ranked matches
   expect_error(
-    backup_link_identifier(example_urls, "quarterly"),
-    class = "read_apra_error_back_url_multiple_matches"
+    check_http_status(
+      url = "https://example.com/servererror",
+      error_message = "Encountered error:"
+    ),
+    regexp = "Encountered error: ",
+    class = "readapra_error_http_status_error"
+  )
+
+  disable(quiet = TRUE)
+})
+
+test_that("get_http_status() returns http details", {
+  enable(quiet = TRUE)
+
+  to_return(.data = stub_request("get", "https://example.com/success"), status = 200)
+  to_return(.data = stub_request("get", "https://example.com/notfound"), status = 404)
+  to_return(.data = stub_request("get", "https://example.com/servererror"), status = 501)
+
+  expect_equal(
+    get_http_status("https://example.com/success"),
+    list(
+      category = "Success",
+      reason = "OK",
+      message = "Success: (200) OK"
+    )
+  )
+
+  expect_equal(
+    get_http_status("https://example.com/notfound"),
+    list(
+      category = "Client error",
+      reason = "Not Found",
+      message = "Client error: (404) Not Found"
+    )
+  )
+
+  expect_equal(
+    get_http_status("https://example.com/servererror"),
+    list(
+      category = "Server error",
+      reason = "Not Implemented",
+      message = "Server error: (501) Not Implemented"
+    )
+  )
+
+  disable(quiet = TRUE)
+})
+
+test_that("download_file() file download", {
+  skip_if_offline()
+  skip_on_cran()
+
+  expect_true(
+    file.exists(
+      polite_file_download(
+        bow = bow_wrapper("https://httpbin.org/bytes/512"),
+        url = "https://httpbin.org/bytes/512", quiet = TRUE
+      )
+    )
+  )
+})
+
+test_that("attempt_polite_file_download() error behaviour", {
+  # Invalid method supplied
+  expect_error(
+    attempt_polite_file_download(
+      url = "dummy_url",
+      bow = "dummy_bow",
+      method = "invalid_method"
+    )
+  )
+
+  # Checking safe fall backs
+  local_mocked_bindings(sys_sleep_wrapper = function(...) NULL)
+  local_mocked_bindings(safe_polite_file_download = function(...) list(result = NULL, error = "Error"))
+  local_mocked_bindings(get_http_status = function(...) list(message = "get_http_status_message"))
+
+  expect_error(
+    attempt_polite_file_download(url = "dummy_url", bow = "dummy_bow"),
+    class = "readapra_error_could_not_download_file"
+  )
+})
+
+test_that("attempt_polite_file_download() real file download", {
+  skip_if_offline()
+  skip_on_cran()
+
+  expect_true(
+    file.exists(
+      attempt_polite_file_download(
+        bow = bow_wrapper("https://httpbin.org/bytes/512"),
+        url = "https://httpbin.org/bytes/512", quiet = TRUE
+      )
+    )
   )
 })
