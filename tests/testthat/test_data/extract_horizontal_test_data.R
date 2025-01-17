7fcf0c3c726df2fa28b9099bb699f2a3f2126859
Jarrod van der Wal <jarvanderwal@gmail.com>
2025-01-17 22:10:27 +1100

    Simplified structure of horizontal_test_data.rds.

--- tests/testthat/test_data/extract_horizontal_test_data.R
+++ tests/testthat/test_data/extract_horizontal_test_data.R
@@ -2,8 +2,11 @@ tidyxl_data <-
   tidyxl::xlsx_cells(test_path("test_data", "horizontal_test_data.xlsx"),
     include_blank_cells = FALSE
   )
+
 formatting_data <-
   tidyxl::xlsx_formats(test_path("test_data", "horizontal_test_data.xlsx"))
+
+formatting_data <- list(local = formatting_data$local)
 
 saveRDS(
   object = tibble::lst(tidyxl_data, formatting_data),
